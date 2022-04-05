d09767a7bb66334eb767ed58de42d0f4910d8535
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2022-04-05 16:58:51 -0400

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,17 @@
+my_num =  25    # Add your code here!
+
+my_boolean =  true   # And here!
+
+my_string =  "Ruby"   # Also here.
+
+puts my_num
+puts my_boolean
+puts my_string
+
+puts "Hello"
+
+print "My name is Shivani!"
+
+"Shivani".length
+
+"Shivani".reverse
